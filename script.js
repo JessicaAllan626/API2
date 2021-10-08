2958d0eca6e2d078f06644c951543612cb77104f
Jessica Allan <JessicaAllan626@gmail.com>
2021-10-07 20:25:12 -0400

    done

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let APIKEY='1oO56iSn72SX3mBjGb1DJl8F4yJGPkiv';
 
     document.addEventListener("DOMContentLoaded", init);
     function init() {
-        document.getElementById("btn").addEventListener("click", ev => {
-        ev.preventDefault(); //to stop the page reload
+        document.getElementsById("btn").addEventListener("click", ev => {
+        ev.preventDefault(); 
         let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
         let str = document.getElementById("search").value.trim();
         url = url.concat(str);
@@ -14,15 +14,17 @@ let APIKEY='1oO56iSn72SX3mBjGb1DJl8F4yJGPkiv';
 
             console.log(content.data);
             console.log("META", content.meta);
+
             let fig = document.createElement("figure");
             let img = document.createElement("img");
             let fc = document.createElement("figcaption");
             img.src = content.data[0].images.downsized.url;
             img.alt = content.data[0].title;
             fc.textContent = content.data[0].title;
+            let out = document.querySelector(".gallery");
+            out.appendChild(fig);
             fig.appendChild(img);
             fig.appendChild(fc);
-            let out = document.querySelector(".out");
             out.insertAdjacentElement("afterbegin", fig);
             document.querySelector("#search").value = "";
             })
